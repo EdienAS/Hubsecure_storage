e02b65abc8af7a6a54c4eaf0d5ad6d6d9e4d2a78
maruthi_edien <maruthi@edien.com>
2023-04-24 20:33:02 +0530

    dashboard folder click fix

--- frontend/src/views/backend/Dashboards/Dashboard.vue
+++ frontend/src/views/backend/Dashboards/Dashboard.vue
@@ -43,40 +43,30 @@
 
       <!-- loopRecentFolders :: start -->
       <div class="col-md-6 col-sm-6 col-lg-3" v-for="folder in getRecentFolders" :key="folder.data.uuid">
-        <div class="card card-block card-stretch card-height hub-folders" v-if="folder.data.type === 'folder'"
-          @dblclick.prevent="showFolderDetails(folder.data.uuid)">
+        <div class="card card-block card-stretch card-height hub-folders" v-if="folder.data.type === 'folder'" @dblclick.prevent="showFolderDetails(folder.data.uuid)">
           <div class="card-body">
             <div class="d-flex justify-content-between">
               <div class="hub-folder mb-4">
                 <svg fill="none" height="50" viewBox="0 0 50 50" width="50" xmlns="http://www.w3.org/2000/svg">
-                  <path
-                    d="m40.6863 14.9414h-18.0392l-3.5883-4.2647c-.1207-.1451-.3016-.2265-.4902-.2206h-14.55389c-2.22206.0162-4.01471 1.8221-4.01471 4.0441v26.2304c.00269608 2.2297 1.80956 4.0365 4.03922 4.0392h36.64708c2.2296-.0027 4.0365-1.8095 4.0392-4.0392v-21.75c-.0027-2.2296-1.8096-4.0365-4.0392-4.0392z"
-                    fill="#4590e8" />
-                  <path
-                    d="m45.9853 9.71582h-18.0441l-3.5883-4.26471c-.1207-.1451-.3016-.22647-.4902-.22059h-14.549c-2.01617.00331-3.72108 1.49277-3.9951 3.4902h13.2696c.6958-.00172 1.3565.30539 1.8039.83823l3.0638 3.64215h17.2304c3.1868.0054 5.7691 2.5876 5.7745 5.7745v20.5294c2.0206-.252 3.5376-1.9686 3.5392-4.0049v-21.75c-.0053-2.2182-1.7966-4.01811-4.0147-4.03428z"
-                    fill="#73b3ff" />
+                  <path d="m40.6863 14.9414h-18.0392l-3.5883-4.2647c-.1207-.1451-.3016-.2265-.4902-.2206h-14.55389c-2.22206.0162-4.01471 1.8221-4.01471 4.0441v26.2304c.00269608 2.2297 1.80956 4.0365 4.03922 4.0392h36.64708c2.2296-.0027 4.0365-1.8095 4.0392-4.0392v-21.75c-.0027-2.2296-1.8096-4.0365-4.0392-4.0392z" fill="#4590e8" />
+                  <path d="m45.9853 9.71582h-18.0441l-3.5883-4.26471c-.1207-.1451-.3016-.22647-.4902-.22059h-14.549c-2.01617.00331-3.72108 1.49277-3.9951 3.4902h13.2696c.6958-.00172 1.3565.30539 1.8039.83823l3.0638 3.64215h17.2304c3.1868.0054 5.7691 2.5876 5.7745 5.7745v20.5294c2.0206-.252 3.5376-1.9686 3.5392-4.0049v-21.75c-.0053-2.2182-1.7966-4.01811-4.0147-4.03428z" fill="#73b3ff" />
                 </svg>
               </div>
               <div class="card-header-toolbar">
-                <b-dropdown id="dropdownMenuButton1" right variant="none p-0" data-toggle="dropdown"
-                  class="dropdown shadow none">
+                <b-dropdown id="dropdownMenuButton1" right variant="none p-0" data-toggle="dropdown" class="dropdown shadow none">
                   <template v-slot:button-content>
                     <span class="dropdown-toggle" id="dropdownMenuButton2" data-toggle="dropdown">
                       <i class="ri-more-fill"></i>
                     </span>
                   </template>
-                  <b-dropdown-item @click="showViewFolderModalFn(folder.data.uuid)"><i class="ri-eye-fill mr-2"></i>{{
-                    "view" }}</b-dropdown-item>
-                  <b-dropdown-item @click="deleteFolderByIdToTrash(folder.data.uuid)"><i
-                      class="ri-recycle-line mr-2"></i>{{ "trash" }}</b-dropdown-item>
-                  <b-dropdown-item @click="deleteFolderByIdPermanently(folder.data.uuid)"><i
-                      class="ri-delete-bin-6-fill mr-2"></i>{{ "delete" }}</b-dropdown-item>
-                  <b-dropdown-item @click="downloadFolderByIdAn(folder.data.uuid)"><i
-                      class="ri-folder-download-fill mr-2"></i>{{ "download" }}</b-dropdown-item>
+                  <b-dropdown-item @click="showViewFolderModalFn(folder.data.uuid)"><i class="ri-eye-fill mr-2"></i>{{ "view" }}</b-dropdown-item>
+                  <b-dropdown-item @click="deleteFolderByIdToTrash(folder.data.uuid)"><i class="ri-recycle-line mr-2"></i>{{ "trash" }}</b-dropdown-item>
+                  <b-dropdown-item @click="deleteFolderByIdPermanently(folder.data.uuid)"><i class="ri-delete-bin-6-fill mr-2"></i>{{ "delete" }}</b-dropdown-item>
+                  <b-dropdown-item @click="downloadFolderByIdAn(folder.data.uuid)"><i class="ri-folder-download-fill mr-2"></i>{{ "download" }}</b-dropdown-item>
                 </b-dropdown>
               </div>
             </div>
-            <router-link to="uploaded-files" class="folder">
+            <div class="folder">
               <h5>{{ folder.data.attributes.name }}</h5>
               <span class="hub-folder-meta">
                 <span class="hf-files" v-if="folder.data.attributes.items">{{ folder.data.attributes.items }} files</span>
@@ -96,7 +86,7 @@
                   </b-avatar-group>
                 </div>
               </template> -->
-            </router-link>
+            </div>
           </div>
         </div>
       </div>
@@ -154,12 +144,8 @@
                 <td class="rt-name">
                   <router-link :to="{ name: 'drive.filebrowser' }" class="rt-link">
                     <svg fill="none" height="20" viewBox="0 0 20 20" width="20" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                        d="m17.2188 5.47656v13.23434c0 .711-.5782 1.2891-1.2891 1.2891h-11.91017c-.71094 0-1.28906-.5781-1.28906-1.2891v-17.42184c0-.710935.57812-1.28906 1.28906-1.28906h7.71877z"
-                        fill="#f5b912" />
-                      <path
-                        d="m12.6406 9.12109h-5.25388c-.48828 0-.88281.39454-.88281.88281v5.2539c0 .4883.39453.8828.88281.8828h5.25388c.4883 0 .8828-.3945.8828-.8828v-5.2578c0-.48438-.3945-.87891-.8828-.87891zm-.0273 5.44921h-5.19533v-3.5351h5.19533z"
-                        fill="#fff" />
+                      <path d="m17.2188 5.47656v13.23434c0 .711-.5782 1.2891-1.2891 1.2891h-11.91017c-.71094 0-1.28906-.5781-1.28906-1.2891v-17.42184c0-.710935.57812-1.28906 1.28906-1.28906h7.71877z" fill="#f5b912" />
+                      <path d="m12.6406 9.12109h-5.25388c-.48828 0-.88281.39454-.88281.88281v5.2539c0 .4883.39453.8828.88281.8828h5.25388c.4883 0 .8828-.3945.8828-.8828v-5.2578c0-.48438-.3945-.87891-.8828-.87891zm-.0273 5.44921h-5.19533v-3.5351h5.19533z" fill="#fff" />
                       <path d="m12.4844 5.25 4.7304 3.83203v-3.60156l-2.6836-1.55469z" fill="#000" opacity=".19" />
                       <path d="m17.2422 5.47656h-4.1875c-.7109 0-1.2891-.57812-1.2891-1.28906v-4.1875z" fill="#fadc87" />
                     </svg>
@@ -185,21 +171,16 @@
                       </b-avatar-group> -->
                       </span>
                     </template>
-                    <b-dropdown id="dropdownMenuButton1" right variant="none p-0" data-toggle="dropdown"
-                      class="dropdown shadow none recent-popup">
+                    <b-dropdown id="dropdownMenuButton1" right variant="none p-0" data-toggle="dropdown" class="dropdown shadow none recent-popup">
                       <template v-slot:button-content>
                         <span class="dropdown-toggle" id="dropdownMenuButton2" data-toggle="dropdown">
                           <i class="ri-more-fill"></i>
                         </span>
                       </template>
-                      <b-dropdown-item @click="showViewFileModalFn(file.data.uuid)"><i class="ri-eye-fill mr-2"></i>{{
-                        "view" }}</b-dropdown-item>
-                      <b-dropdown-item @click="deleteFileByIdToTrash(file.data.uuid)"><i
-                          class="ri-recycle-line mr-2"></i>{{ "trash" }}</b-dropdown-item>
-                      <b-dropdown-item @click="deleteFileByIdPermanently(file.data.uuid)"><i
-                          class="ri-delete-bin-6-fill mr-2"></i>{{ "delete" }}</b-dropdown-item>
-                      <b-dropdown-item @click="downloadFileById({ base_name: file.data.attributes.basename })"><i
-                          class="ri-folder-download-fill mr-2"></i>{{ "download" }}</b-dropdown-item>
+                      <b-dropdown-item @click="showViewFileModalFn(file.data.uuid)"><i class="ri-eye-fill mr-2"></i>{{ "view" }}</b-dropdown-item>
+                      <b-dropdown-item @click="deleteFileByIdToTrash(file.data.uuid)"><i class="ri-recycle-line mr-2"></i>{{ "trash" }}</b-dropdown-item>
+                      <b-dropdown-item @click="deleteFileByIdPermanently(file.data.uuid)"><i class="ri-delete-bin-6-fill mr-2"></i>{{ "delete" }}</b-dropdown-item>
+                      <b-dropdown-item @click="downloadFileById({ base_name: file.data.attributes.basename })"><i class="ri-folder-download-fill mr-2"></i>{{ "download" }}</b-dropdown-item>
                     </b-dropdown>
                   </span>
                 </td>
@@ -215,8 +196,7 @@
           <div v-if="getRightSideFileDetail" class="mx-auto">
             <div>
               <div class="d-flex align-items-start justify-content-start">
-                <b-img fluid-grow :src="getRightSideFileDetail[0].data.attributes.thumbnail.sm"
-                  :alt="getRightSideFileDetail[0].data.attributes.name" />
+                <b-img fluid-grow :src="getRightSideFileDetail[0].data.attributes.thumbnail.sm" :alt="getRightSideFileDetail[0].data.attributes.name" />
               </div>
               <div>
                 <p>{{ getRightSideFileDetail[0].data.attributes.name }}</p>
@@ -252,8 +232,7 @@
             </div>
           </div>
           <b-card class="storage-card">
-            <b-progress :value="fileTraffic?.total_uploaded_percent" :max="100" height="40px"
-              class="mb-3 hub-storage"></b-progress>
+            <b-progress :value="fileTraffic?.total_uploaded_percent" :max="100" height="40px" class="mb-3 hub-storage"></b-progress>
             <div class="storage-legend d-flex justify-content-between">
               <span class="active sl-legend">
                 <i class="ri-checkbox-blank-circle-fill"></i>
@@ -263,8 +242,7 @@
               <span class="deactive sl-legend">
                 <i class="ri-checkbox-blank-circle-fill"></i>
                 <span class="sl-title">Storage Left</span>
-                <span class="sl-value">{{ ((fileTraffic?.total_storage - fileTraffic?.total_uploaded) *
-                  0.00000095367432).toPrecision(4) }} MB</span>
+                <span class="sl-value">{{ ((fileTraffic?.total_storage - fileTraffic?.total_uploaded) * 0.00000095367432).toPrecision(4) }} MB</span>
               </span>
             </div>
           </b-card>
